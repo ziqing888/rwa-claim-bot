ec82ed4647087d6df1a269b01d6089a78b13478b
子清 <120438383+ziqing888@users.noreply.github.com>
2024-10-31 14:25:18 +0800

    Create proxy_list_tmp.js

---
+++ config/proxy_list_tmp.js
@@ -0,0 +1,10 @@
+/**
+ * 示例：
+ * export const proxyList = [
+ *    "http://代理用户:代理密码@代理主机:代理端口",
+ *    "http://代理用户:代理密码@代理主机:代理端口",
+ *    "http://代理用户:代理密码@代理主机:代理端口",
+ * ];
+ */
+
+export const proxyList = [];
